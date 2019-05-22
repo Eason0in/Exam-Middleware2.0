9946f39d2673bc9c045b60448c9df8e54668da2a
Eason <leoling127@gmail.com>
2019-05-22 23:23:39 +0800

    Fix to one middleware

--- app.js
+++ app.js
@@ -8,48 +8,47 @@ const requestData = (req, res, next) => {
   res.locals.path = req.url
   res.locals.method = req.method
   next()
+
+  req.on('end', () => {
+    if (req.originalUrl.indexOf('favicon.ico') === -1) {
+      const { startDate, path, method } = res.locals
+      const nowDate = new Date()
+      const times = nowDate - startDate
+      console.log(`${startDate.toLocaleString()} | ${method} From ${path} | Total time: ${times}ms`)
+    }
+  })
 }
 
-const responseData = (req, res) => {
-  if (req.originalUrl.indexOf('favicon.ico') === -1) {
-    const { startDate, path, method } = res.locals
-    const nowDate = new Date()
-    const times = nowDate - startDate
-    console.log(`${startDate.toLocaleString()} | ${method} From ${path} | Total time: ${times}ms`)
-  }
-}
-
-app.use(requestData)
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // 列出全部 Todo
-app.get('/', (req, res) => {
+app.get('/', requestData, (req, res) => {
   res.render('index', { message: '顯示所有Todo' })
-  responseData(req, res)
 })
 
 // 新增一筆 Todo 頁面
-app.get('/new', (req, res) => {
+app.get('/new', requestData, (req, res) => {
   res.render('index', { message: '新增 Todo 頁面' })
-  responseData(req, res)
+
+  // responseData(req, res)
 })
 
 // 顯示一筆 Todo 的詳細內容
-app.get('/:id', (req, res) => {
+app.get('/:id', requestData, (req, res) => {
   res.render('index', { message: '顯示一筆Todo' })
-  responseData(req, res)
+  // responseData(req, res)
 })
 
 // 新增一筆  Todo
-app.post('/', (req, res) => {
+app.post('/', requestData, (req, res) => {
   res.render('index', { message: 'POST-新增一筆 Todo' })
-  responseData(req, res)
+  // responseData(req, res)
 })
 
-app.post('/:id/delete', (req, res) => {
+app.post('/:id/delete', requestData, (req, res) => {
   res.render('index', { message: 'POST-刪除 Todo' })
-  responseData(req, res)
+  // responseData(req, res)
 })
 
 app.listen(port, () => {
